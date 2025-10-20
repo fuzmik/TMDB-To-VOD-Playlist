73568482ff1a87d560bf70350b5ca8e1b28d0b00
fuzmik <sljbbr@gmail.com>
2025-10-20 14:58:18 -0700

    update

--- Dockerfile.php
+++ Dockerfile.php
@@ -25,7 +25,7 @@ RUN docker-php-ext-configure gd --with-freetype --with-jpeg --with-webp \
     && docker-php-ext-install -j$(nproc) gd \
     && docker-php-ext-install -j$(nproc) zip \
     && docker-php-ext-install -j$(nproc) curl \
-    && docker-php-ext-install -j$(nproc) mbstring \
+    && docker-php-ext-enable mbstring \
     && docker-php-ext-install -j$(nproc) openssl \
     && docker-php-ext-install -j$(nproc) json \
     && docker-php-ext-install -j$(nproc) xml \
